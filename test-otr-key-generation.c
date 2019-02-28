05a6e1e4fb609e489cbcd8700912dae6500eca7f
Boris Pek <tehnick-8@yandex.ru>
2019-03-01 00:44:36 +0300

    Add otr key generation.

--- test-otr-key-generation.c
+++ test-otr-key-generation.c
@@ -23,7 +23,13 @@
 
 #include <stdio.h>
 #include <libotr/proto.h>
+#include <libotr/instag.h>
+#include <libotr/privkey.h>
 #include <libotr/userstate.h>
+
+#ifndef OTRL_PRIVKEY_FPRINT_HUMAN_LEN
+#define OTRL_PRIVKEY_FPRINT_HUMAN_LEN 45
+#endif
 
 int main()
 {
@@ -40,6 +46,46 @@ int main()
     userstate = otrl_userstate_create();
 
     printf("Label 3\n"); fflush(stdout);
+
+    const char *accountname = "random_jid@example.com";
+    const char *protocol = "prpl-jabber";
+    const char *keys_file = "otr.keys";
+
+    void *newkeyp = NULL;
+    if (otrl_privkey_generate_start(userstate, accountname, protocol, &newkeyp) == gcry_error(GPG_ERR_EEXIST)) {
+        printf("libotr reports it's still generating a previous key while it shouldn't be"); fflush(stdout);
+        return 1;
+    }
+
+    printf("Label 4\n"); fflush(stdout);
+
+    if (otrl_privkey_generate_calculate(newkeyp) == gcry_error(GPG_ERR_NO_ERROR)) {
+        otrl_privkey_generate_finish(userstate, newkeyp, keys_file);
+    }
+    else {
+        printf("Kill all humans!111\n"); fflush(stdout);
+        return 1;
+    }
+
+    printf("Label 5\n"); fflush(stdout);
+
+    char fingerprint[OTRL_PRIVKEY_FPRINT_HUMAN_LEN];
+    if (otrl_privkey_fingerprint(userstate, fingerprint, accountname, protocol))
+    {
+        printf("Keys have been generated. Fingerprint for account \"%s\":\n"
+               "%s\n"
+               "Thanks for your patience!\n",
+               accountname,
+               fingerprint
+              );
+        fflush(stdout);
+    }
+    else
+    {
+        printf("Failed to generate keys for account \"%s\".", accountname);
+        fflush(stdout);
+        return 1;
+    }
 
     otrl_userstate_free(userstate);
 
